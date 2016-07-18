72a17acc99a06c65eabae1eb41be99b9e2e00fcb
Eric Anders <eric@stuckshut.com>
2016-07-18 11:17:06 -0500

    add test for application ID

--- src/teahelper_lambda.py
+++ src/teahelper_lambda.py
@@ -9,6 +9,9 @@ def lambda_handler(event, context):
     session = Session(event['session'])
     request = Request(event['request'])
 
+    if session.applicationId != 'amzn1.echo-sdk-ams.app.43de1112-c924-46eb-b053-b8a410268e36':
+        return on_error(request, session).to_dict()
+
     if request.type == 'LaunchRequest':
         handler = on_launch
     elif request.type == 'IntentRequest':
