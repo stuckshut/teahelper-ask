505ae010e3d8a2b34336e6de2842ee8a61e4bfc3
Eric Anders <eric@stuckshut.com>
2016-07-18 12:43:52 -0500

    add the reprompt outputSpeech in a way that actually works

--- src/response.py
+++ src/response.py
@@ -47,8 +47,10 @@ class Response(object):
             'text': message
         }
         response.reprompt = {
-            'type': 'PlainText',
-            'text': message
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': message
+            }
         }
         response.shouldEndSession = False
 
@@ -68,7 +70,7 @@ class Response(object):
             out['response']['card'] = self.card
 
         if self.reprompt is not None:
-            out['response']['reprompt']['outputSpeech'] = self.reprompt
+            out['response']['reprompt'] = self.reprompt
 
         if self.sessionAttributes is not None:
             out['sessionAttributes'] = self.sessionAttributes
